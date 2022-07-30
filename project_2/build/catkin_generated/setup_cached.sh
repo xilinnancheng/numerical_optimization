7134f5713ffd8cca954b05a8d1f8b0affd408c07
harrison <46555440+xilinnancheng@users.noreply.github.com>
2022-07-30 18:55:45 +0800

    Update README.md

---
+++ project_2/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/hongrunchen/Downloads/numerical_optimization/project_2/devel:/opt/ros/noetic'
+export LD_LIBRARY_PATH='/opt/ros/noetic/lib'
+export PKG_CONFIG_PATH='/opt/ros/noetic/lib/pkgconfig'
+export PWD='/home/hongrunchen/Downloads/numerical_optimization/project_2/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/hongrunchen/Downloads/numerical_optimization/project_2/devel/share/common-lisp'
+export ROS_PACKAGE_PATH='/home/hongrunchen/Downloads/numerical_optimization/project_2/src:/opt/ros/noetic/share'
---
+++ project_2/build/gcopter/CMakeFiles/curve_gen.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hongrunchen/Downloads/numerical_optimization/project_2/src/gcopter/src/curve_gen.cpp" "/home/hongrunchen/Downloads/numerical_optimization/project_2/build/gcopter/CMakeFiles/curve_gen.dir/src/curve_gen.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"gcopter\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/ros/noetic/include"
+  "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/usr/include/eigen3"
+  "/home/hongrunchen/Downloads/numerical_optimization/project_2/src/gcopter/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
