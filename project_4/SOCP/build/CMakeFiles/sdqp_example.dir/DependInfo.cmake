0b95d63d4deff15ec6588f0c57950b645ce3b9cc
hongrun.chen <hongrun.chen@weride.ai>
2022-08-21 16:14:51 +0800

    project_4

---
+++ project_4/SOCP/build/CMakeFiles/sdqp_example.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hongrunchen/Downloads/numerical_optimization/project_4/SOCP/src/example/socp_example.cpp" "/home/hongrunchen/Downloads/numerical_optimization/project_4/SOCP/build/CMakeFiles/sdqp_example.dir/example/socp_example.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/hongrunchen/Downloads/numerical_optimization/project_4/SOCP/src/include"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project_4/SOCP/build/CMakeFiles/socp_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/socp_example.dir/example/socp_example.cpp.o"
+  "socp_example"
+  "socp_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/socp_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
